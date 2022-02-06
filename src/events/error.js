eb6b78875926fa1c89d0121e71e7ecc60e5e4926
ipincamp <arif.lngsrn@gmail.com>
2022-02-06 19:16:18 +0700

    Add more event (warn & error)

---
+++ src/events/error.js
@@ -0,0 +1,13 @@
+/**
+ * @name blckdot
+ * @license GPL-3.0
+ * @version v1.0.0
+ * @author ipincamp <support@nur-arifin.my.id>
+ */
+
+module.exports = {
+	name: "error",
+	execute(err) {
+		console.error(err);
+	},
+};
---
+++ src/events/warn.js
@@ -0,0 +1,13 @@
+/**
+ * @name blckdot
+ * @license GPL-3.0
+ * @version v1.0.0
+ * @author ipincamp <support@nur-arifin.my.id>
+ */
+
+module.exports = {
+	name: "warn",
+	execute(info) {
+		console.info(info);
+	},
+};
