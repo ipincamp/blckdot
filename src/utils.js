1c3c35e6e9b5cdaa1b524524546a9fb0c9f69acc
ipincamp <arif.lngsrn@gmail.com>
2022-02-06 20:02:27 +0700

    Config modules, there is 2 way to use.
    You can use json file (config.json), or use dotenv (.env)

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+/**
+ * @name blckdot
+ * @license GPL-3.0
+ * @version v1.0.0
+ * @author ipincamp <support@nur-arifin.my.id>
+ */
+
+let config;
+
+try {
+	config = require("./config.json");
+} catch (error) {
+	config = null;
+}
+
+exports.token = config ? config.token : process.env.TOKEN;
+exports.prefix = config ? config.prefix : process.env.PREFIX;
