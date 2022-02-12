785e6444354072bbd5e82b62db40b2ba49a6d9ef
ipincamp <arif.lngsrn@gmail.com>
2022-02-12 11:15:52 +0700

    Soon i'll make loggers
    1. Extract commands handler into handlers.js.
    2. Reusage client.prefix on arrayPresence.

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,53 +4,23 @@
  * @version v1.0.0
  * @author ipincamp <support@nur-arifin.my.id>
  */
-
-const fs = require("fs");
-
-const { prefix } = require("../utils/utils");
-
 module.exports = {
 	name: "ready",
 	once: true,
 	execute(client) {
-		console.info(`${client.user.tag} ready!`);
-
 		const arrayPresence = [
-			`${prefix}help`,
+			`${client.prefix}help`,
 			"Spotify",
 			"YouTube",
 		];
 
 		setInterval(() => {
 			const index = Math.floor(Math.random() * (arrayPresence.length - 1) + 1);
+
 			client.user.setActivity(arrayPresence[index], { type: "LISTENING" });
 			client.user.setStatus("idle");
 		}, 10000);
 
-		/**
- 		* Commands Handler
- 		*/
-		fs.readdir("./src/commands/", (e, f) => {
-			if (e) {
-				console.error(e);
-			} else {
-				console.info(`Register ${f.length} categories!`);
-				f.forEach((g) => {
-					fs.readdir(`./src/commands/${g}`, (h, i) => {
-						console.info(`> ${i.length} commands from ${g}`);
-						if (h) {
-							console.error(h);
-						} else {
-							i.forEach((j) => {
-								if (!j.endsWith(".js")) return;
-								let command = require(`../commands/${g}/${j}`);
-
-								client.commands.set(command.name, command);
-							});
-						}
-					});
-				});
-			}
-		});
+		console.info(`${client.user.tag} ready!`);
 	},
 };
