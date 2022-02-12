621d6bfb60bc5a395aa8a3b1d784c0fff5e6c815
ipincamp <arif.lngsrn@gmail.com>
2022-02-12 11:19:47 +0700

    Update configuration
    1. Exported defaultVolume, playlistSize and youtubeAPI.
    2. Also you'll be optional to store config into dotenv.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -14,8 +14,11 @@ try {
 }
 
 exports.canModify = (member) => member.voice.channelId === member.guild.voice.channelID;
+exports.defaultVolume = config ? config.defaultVolume : process.env.DEFAULT_VOLUME || 100;
 exports.dev = config ? config.dev : process.env.DEV;
+exports.playlistSize = config ? config.playlistSize : process.env.PLAYLIST_SIZE || 10;
 exports.prefix = config ? config.prefix : process.env.PREFIX || "p";
 exports.pruning = config ? config.pruning : process.env.PRUNING;
 exports.stayTime = config ? config.stayTime : process.env.STAY_TIME;
 exports.token = config ? config.token : process.env.TOKEN;
+exports.youtubeAPI = config ? config.youtubeAPI : process.env.YOUTUBE_API;
