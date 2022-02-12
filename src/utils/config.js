4dcecd154549d55463b711c45a97deb211c7c754
ipincamp <arif.lngsrn@gmail.com>
2022-02-12 16:41:27 +0700

    Refactoring config file
    1. Deprecated unused configuration.
    2. Add loop module.

---
+++ src/utils/config.js
@@ -0,0 +1,19 @@
+/**
+ * @name blckdot
+ * @license GPL-3.0
+ * @version v1.0.0
+ * @author ipincamp <support@nur-arifin.my.id>
+ */
+
+let config;
+
+try {
+	config = require("../config.json");
+} catch (error) {
+	config = null;
+}
+
+exports.dev = config ? config.dev : process.env.DEV;
+exports.loop = config ? config.loop : process.env.LOOP || false;
+exports.prefix = config ? config.prefix : process.env.PREFIX || "p";
+exports.token = config ? config.token : process.env.TOKEN;
